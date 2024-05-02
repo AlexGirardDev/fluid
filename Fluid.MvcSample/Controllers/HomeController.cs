c1a5f49371ecb7c9c69f7517fc93ae497dce4732
Alex Girard <me@alexgirard.ca>
2024-05-02 11:53:12 -0400

    broken MVC sample

--- Fluid.MvcSample/Controllers/HomeController.cs
+++ Fluid.MvcSample/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace Fluid.MvcSample.Controllers
             var model = new List<Person>();
             model.Add(new Person { Firstname = "Bill", Lastname = "Gates" });
             model.Add(new Person { Firstname = "Steve", Lastname = "Balmer" });
-            
+            model.Add(new Person {Firstname = new string(Enumerable.Range(0, 5_000).Select(_ => 'a').ToArray()), Lastname = "Gates"});
             ViewData["Title"] = "This is a title";
 
             return View(model);
